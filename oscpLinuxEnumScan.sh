87ebd3ec1273f58eab4c9c6c78e1af2f8ae3d2d4
fancy-sauce <60056952+fancy-sauce@users.noreply.github.com>
2025-08-16 15:59:23 -0700

    Update oscpLinuxEnumScan.sh
    Adding a few more commands for kernel enumeration, reorganize based on simple to more detailed info, etc.

--- oscpLinuxEnumScan.sh
+++ oscpLinuxEnumScan.sh
@@ -12,23 +12,21 @@ else
     echo "---------User is NOT a sudoer. Skipping privileged commands."
 fi
 
-
 echo "---------current user info"
 id
-
-echo "---------Passwd file"
-cat /etc/passwd
 
 echo "---------check hostname"
 hostname
 
-echo "---------shadow file"
-ls -l /etc/shadow
-
 echo "---------OS info"
 cat /etc/issue
-
 uname -a
+
+echo "---------Passwd file"
+cat /etc/passwd
+
+echo "---------shadow file"
+ls -l /etc/shadow
 
 echo "---------processes, check for Root owned processes that are vulnerable"
 ps aux
@@ -48,9 +46,7 @@ ls -lah /etc/cron*
 echo "---------Listing cron jobs for the current user"
 crontab -l
 
-
-
-echo "---------Listing all installed packages on a Debian Linux operating system"
+echo "---------Listing all Debian installed packages"
 dpkg -l
 
 echo "---------Listing all world writable directories"
@@ -68,3 +64,43 @@ lsmod
 
 echo "---------Searching for SUID files...Try linpeas.sh for more detail on vulns, and GTFOBins for exploits"
 find / -perm -u=s -type f 2>/dev/null
+
+echo "---------Inspecting Environment Variables"
+env
+
+cat .bashrc
+
+echo "---------Harvesting Active Processes for Credentials"
+watch -n 1 "ps -aux | grep pass"
+
+echo "---------Using tcpdump to Perform Password Sniffing"
+sudo tcpdump -i lo -A | grep "pass"
+
+echo "---------Inspecting the cron log file"
+grep "CRON" /var/log/syslog
+
+echo "---------Enumerating Linux Capabilities, another good place to check binary SUIDs to GTFOBins"
+/usr/sbin/getcap -r / 2>/dev/null
+
+echo "---------Gathering general information on the target system"
+cat /etc/issue
+
+
+echo "---------Gathering kernel and architecture information"
+uname -r
+
+arch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
